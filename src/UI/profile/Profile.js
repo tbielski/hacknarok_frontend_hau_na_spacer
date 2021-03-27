2491eea01c9dcdcb6d43dd1d6de971aaf2d287fb
Dawid Dereszkiewicz <72603663+ddereszkiewicz@users.noreply.github.com>
2021-03-27 18:41:43 +0100

    szkielet profile info

---
+++ src/UI/profile/Profile.js
@@ -0,0 +1,42 @@
+import React from "react";
+import Paper from "@material-ui/core/Paper";
+import { Grid, makeStyles } from "@material-ui/core";
+import ProfileInfo from "./ProfileInfo/ProfileInfo";
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: "flex",
+        flexWrap: "wrap",
+        "& > *": {
+            margin: theme.spacing(1),
+            width: theme.spacing(16),
+            height: theme.spacing(16),
+        },
+    },
+    paper: {
+        marginTop: theme.spacing(5),
+    },
+}));
+const user = {
+    userId: 0,
+    firstName: "Janusz",
+    lastName: "Kowalski",
+    email: "jkowalski@gmail.com",
+
+    rating: 4.2,
+    describe: "Jestem wlascicielem super slodkiego Amstafa",
+    age: 48,
+    usersToRate: [2],
+};
+const Profile = () => {
+    const classes = useStyles();
+    return (
+        <Grid container justify="center" direction="row">
+            <Grid item sm={8} container>
+                <Grid className={classes.paper}>
+                    <ProfileInfo user={user} />
+                </Grid>
+            </Grid>
+        </Grid>
+    );
+};
+export default Profile;
